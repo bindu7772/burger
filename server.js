372c99b21ec497585aa9acf22c2be6c6367b74e3
bindu7772 <manubpatel4@gmail.com>
2017-11-03 22:58:36 -0400

    js file change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+var method_override = require("method-override");
 var port = process.env.PORT || 3000;
 
 var app = express();
@@ -9,15 +9,15 @@ var app = express();
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 // Set Handlebars.
 var exphbs = require("express-handlebars");
+var hbs = require("handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
