63fbdaebbb96eee0f2de09cc7dd78abdf9e15713
bindu7772 <manubpatel4@gmail.com>
2017-11-16 20:56:15 -0500

    heroku db

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 // create the connection information for the sql database
 var connection;
 
-if(process.env.JAWsDB_URL){
-	connection = mysql.createConnection(process.env.JAWsDB_URL);
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
 	connection = mysql.createConnection({
 		host: 'localhost',
